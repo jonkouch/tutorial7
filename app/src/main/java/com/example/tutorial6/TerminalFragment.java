72244d523d2375c35089961bcdd78f253c205da6
jonkouch <102368425+jonkouch@users.noreply.github.com>
2023-05-21 13:03:11 +0300

    fixing order in writing to csv

--- app/src/main/java/com/example/tutorial6/TerminalFragment.java
+++ app/src/main/java/com/example/tutorial6/TerminalFragment.java
@@ -409,7 +409,7 @@ public class TerminalFragment extends Fragment implements ServiceConnection, Ser
                             CSVWriter csvWriter = new CSVWriter(new FileWriter(csv, true));
 
                             // parse string values, in this case [0] is tmp & [1] is count (t)
-                            String row[] = new String[]{parts[0], parts[1], parts[2], parts[3]};
+                            String row[] = new String[]{parts[3], parts[0], parts[1], parts[2]};
                             csvWriter.writeNext(row);
                             csvWriter.close();
 
